1b14aadacb677f27fc832175823a546d834af487
devmise <74214059+devmise@users.noreply.github.com>
2023-03-30 19:05:44 +0100

    Test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,8 @@
-package org.example;public class Main {
+package org.example;
+
+//testing!!!!
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
