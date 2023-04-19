918429344d3ee6a1052563ef92c1e11f00834f35
devmise <74214059+devmise@users.noreply.github.com>
2023-04-19 12:43:52 +0100

    Query findMovieById added

--- src/main/java/org/example/MovieDaoInterface.java
+++ src/main/java/org/example/MovieDaoInterface.java
@@ -5,4 +5,6 @@ import java.util.List;
 public interface MovieDaoInterface {
 
     public List<Movie> findAllMovies() throws DaoException;
+    public Movie findMovieById(int movie_id) throws DaoException;
+    public Movie deleteMovieById(int movie_id) throws DaoException;
 }
--- src/main/java/org/example/MySqlMovieDao.java
+++ src/main/java/org/example/MySqlMovieDao.java
@@ -7,10 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MySqlMovieDao extends MySqlDao implements MovieDaoInterface{
+public class MySqlMovieDao extends MySqlDao implements MovieDaoInterface
+{
 
     @Override
-    public List<Movie> findAllMovies() throws DaoException {
+    public List<Movie> findAllMovies() throws DaoException
+    {
         Connection connection = null;
         PreparedStatement ps = null;
         ResultSet resultSet = null;
@@ -62,4 +64,64 @@ public class MySqlMovieDao extends MySqlDao implements MovieDaoInterface{
         }
         return movieList;     // may be empty
     }
+
+    @Override
+    public Movie findMovieById(int movie_id) throws DaoException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        Movie movie = null;
+
+        try
+        {
+            connection = this.getConnection();
+
+            String query = "SELECT * FROM MOVIES WHERE MOVIE_ID = ?";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, movie_id);
+
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next())
+            {
+                movie_id = resultSet.getInt("movie_id");
+                String movie_name = resultSet.getString("movie_name");
+                String director = resultSet.getString("director");
+                float rating = resultSet.getFloat("rating");
+                int release_year = resultSet.getInt("release_year");
+                movie  = new Movie(movie_id, movie_name, director, rating, release_year);
+
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DaoException("findMovieById()" + e.getMessage());
+        }
+        finally {
+            try {
+                if (resultSet != null)
+                {
+                    resultSet.close();
+                }
+                if (preparedStatement != null)
+                {
+                    preparedStatement.close();
+                }
+                if (connection != null)
+                {
+                    freeConnection(connection);
+                }
+            }
+            catch (SQLException e)
+            {
+                throw new DaoException("findUserByUsernamePassword() " + e.getMessage());
+            }
+        }
+        return movie;
+    }
+
+    @Override
+    public Movie deleteMovieById(int movie_id) throws DaoException {
+        return null;
+    }
+
 }
