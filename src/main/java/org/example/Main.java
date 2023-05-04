7a6902406a3f87676ca6fbf93e6cddacdb64fe3f
devmise <74214059+devmise@users.noreply.github.com>
2023-05-04 16:13:48 +0100

    Have all 3 queries now running without having to comment anything out

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,37 +9,39 @@ public class Main {
     {
         MovieDaoInterface IMovieDao = new MySqlMovieDao();
 
-        try
-        {
+        try {
             System.out.println("\nCall findAllMovies");
             List<Movie> movies = IMovieDao.findAllMovies();
 
-            if(movies.isEmpty())
+            if (movies.isEmpty())
                 System.out.println("There are no movies");
-            else{
-                for(Movie movie : movies)
+            else {
+                for (Movie movie : movies)
                     System.out.println("Movies: " + movie.toString());
             }
-//
-//            System.out.println("\nCall: findMovieById");
-//            int movie_id = 5;
-//            Movie movie = IMovieDao.findMovieById(movie_id);
-//
-//            if( movie != null)
-//                System.out.println("Movie found: " + movie);
-//            else
-//                System.out.println("Movie with that id was not found");
+
+            {
+                System.out.println("\nCall: findMovieById");
+                int movie_id = 5;
+                Movie movie = IMovieDao.findMovieById(movie_id);
+
+                if (movie != null)
+                    System.out.println("Movie found: " + movie);
+                else
+                    System.out.println("Movie with that id was not found");
+            }
+
+            {
+                System.out.println("\nCall deleteMovieById");
+                int movie_id = 15;
+                Movie movie = IMovieDao.deleteMovieById(movie_id);
 
 
-        System.out.println("\nCall deleteMovieById");
-        int movie_id = 15;
-        Movie movie = IMovieDao.deleteMovieById(movie_id);
-
-
-        if(movie == null)
-            System.out.println("Movie with id = " + movie_id + " successfully deleted");
-        else
-            System.out.println("Movie with that id was not found");
+                if (movie == null)
+                    System.out.println("Movie with id = " + movie_id + " successfully deleted");
+                else
+                    System.out.println("Movie with that id was not found");
+            }
         }
 
         catch (DaoException e)
